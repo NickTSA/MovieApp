fc2d6ca64e7e2706c6ef76719718f72b2c37e388
Narek Tsaturyan <Tnarek77@gmail.com>
2020-06-21 01:56:26 -0700

    build ready

---
+++ build/precache-manifest.d529c1f79ae7c138cf7a21d3f9eec05a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61e13952ac40c3a1f27f3475d55079a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b05258df2fee96ec92e8",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f6b7d0cc6c708516b25e",
+    "url": "/static/css/main.b185402d.chunk.css"
+  },
+  {
+    "revision": "b05258df2fee96ec92e8",
+    "url": "/static/js/2.4048f231.chunk.js"
+  },
+  {
+    "revision": "619f5908e571ce4f7ef947732bc44c31",
+    "url": "/static/js/2.4048f231.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6b7d0cc6c708516b25e",
+    "url": "/static/js/main.c2597b2f.chunk.js"
+  },
+  {
+    "revision": "419d296c21600b90fe58",
+    "url": "/static/js/runtime-main.dfac0e00.js"
+  },
+  {
+    "revision": "d1b4253f16e6f9d9cf9cc5a0af03da24",
+    "url": "/static/media/moviePlaceholder.d1b4253f.png"
+  }
+]);
